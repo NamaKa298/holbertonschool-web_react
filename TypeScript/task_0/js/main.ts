4f192cb356c278819bd9646a0feb8f5a91551d89
NamaKa298 <marion.saint-martin@protonmail.com>
2024-09-24 09:59:53 +0200

    0. Creating an interface for a student

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -1,4 +1,4 @@
-export interface Student {
+interface Student {
     firstName: string;
     lastName: string;
     age: number;
@@ -22,6 +22,13 @@ const student2: Student = {
 const studentsList: Student[] = [student1, student2];
 
 const table = document.createElement('table');
+
+const headerRow = table.insertRow();
+const headerCell1 = headerRow.insertCell(0);
+const headerCell2 = headerRow.insertCell(1);
+headerCell1.textContent = "First Name";
+headerCell2.textContent = "Location";
+
 studentsList.forEach(student => {
     const row = table.insertRow();
     const cell1 = row.insertCell(0);
@@ -31,3 +38,5 @@ studentsList.forEach(student => {
 });
 
 document.body.appendChild(table);
+
+console.log("No type errors found")
